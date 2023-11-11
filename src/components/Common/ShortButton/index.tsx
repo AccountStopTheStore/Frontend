e801a06441c566bf897be2fe3a2879b0c560710b
Sojung Park <sojung6458@gmail.com>
2023-11-11 14:10:47 +0900

    🎨 [STYLE] ShortButton 컴포넌트 코드 포맷 변경

--- src/components/Common/ShortButton/index.tsx
+++ src/components/Common/ShortButton/index.tsx
@@ -6,12 +6,24 @@ interface ShortButtonProps {
 }
 
 function ShortButton({ isSaveButton, onClick }: ShortButtonProps) {
+  const deleteAccountButton = {
+    width: "126px",
+  };
+  const deleteContainerPadding = {
+    padding: "30px",
+  };
+
+  const deleteStyle = isSaveButton ? undefined : deleteAccountButton;
+  const deleteContainerStyle = isSaveButton
+    ? undefined
+    : deleteContainerPadding;
+
   return (
-    <ShortButtonUI.Container>
-      <ShortButtonUI.CancelButton onClick={onClick}>
+    <ShortButtonUI.Container style={deleteContainerStyle}>
+      <ShortButtonUI.CancelButton onClick={onClick} style={deleteStyle}>
         취소
       </ShortButtonUI.CancelButton>
-      <ShortButtonUI.GreenButton onClick={onClick}>
+      <ShortButtonUI.GreenButton onClick={onClick} style={deleteStyle}>
         {isSaveButton ? "저장" : "탈퇴"}
       </ShortButtonUI.GreenButton>
     </ShortButtonUI.Container>
