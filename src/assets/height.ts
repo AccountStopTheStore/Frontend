53d6cc40cc0ac07d8d38a039e555b600890a90bb
Whale2200d <skatewang99@gmail.com>
2023-11-13 16:20:28 +0900

    Feat : 가계부 일일 컴포넌트 생성

---
+++ src/assets/height.ts
@@ -0,0 +1,7 @@
+export const ApplicationHeight = 844;
+
+export const HeaderHeight = 77;
+export const ChosenYearMonthHeight = 53;
+export const FinancialSummaryHeight = 70;
+export const CDMSTabItemsHeight = 50;
+export const NavigationItemsHeight = 70;
